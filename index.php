3db7c016c3b3e8640fae3dfe48a03f449eec41a5
Pablo <pablogahh@gmnail.com>
2026-08-18 20:39:40 -0300

    feat: commit inicial da landing page de estética automotiva

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ursin´s Wash</title>
+   <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="shortcut icon" type="image-x/png" href="logomoto.ico" />
+</head>
+<body>
+  <nav class="light-blue darken-1" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">URSINS_WASH</a>
+      <ul class="right hide-on-med-and-down">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div class="section light-blue darken-2">
+    <div class="container sect1">
+      <div class="form-container">
+        <div class="row">
+          <div class="col s12 center-align">
+            <form>
+              <div class="input-field">
+                <input placeholder="" id="first_name" type="text" class="validate">
+                <label for="first_name">Insira seu e-mail:</label>
+              </div><!--input-field-->
+                <div class="input-field">
+                  <button class="btn waves-effect waves-light light-blue darken-2 " type="submit" name="acion">Enviar
+                  <i class="material-icons right">near_me</i>
+                  </button>
+                </div><!--input-field-->
+            </form>
+          </div><!--col-s12-center-align-->
+        </div><!--row-->
+      </div><!--form-container-->
+    </div><!--container-sect1-->
+  </div>
+ 
+  <div class="section">
+    <div class="container">
+     <div class="row">
+      <div class="col s12 m6">
+        <div class="custom-title">
+          <div></div>
+          <h2>Quem sou eu?</h2>
+          </div><!--custom-title-->
+          <p class="flow-text"><i class="material-icons left">check</i>Sou detalhista e minucioso em todas as áreas, sempre em busca de produtos de alta qualidade;</br><i class="material-icons left">check</i> Uso apenas produtos específicos e eficazes, visando sempre preservação da pintura e do interior;</br><i class="material-icons left">check</i>
+Possuo várias habilidades especializadas e técnicas profissionais para polimento e limpeza e sem contar que fazemos serviços personalizados e adaptados às necessidades individuais do cliente.</p>
+       </div><!--col-s12-m6-->
+
+       <div class="col s12 m6">
+        <div class="custom-title">
+          <div></div>
+          <h2>Mensagem do dia</h2>
+          </div><!--custom-title-->
+          <div class=" card light-blue darken-2 z-depth">
+            <div class="card-content white-text">
+              <span class="card-title"> Lembre-se</span>
+                <p>Na jornada da vida, encontre tempo para apreciar a jornada, não apenas o destino. As pequenas alegrias, os momentos de calma e as conexões significativas moldam nossa existência tanto quanto os grandes marcos. Cultive gratidão pelas experiências simples e pelas lições difíceis, pois todas contribuem para a pessoa extraordinária que você está se tornando. Em cada amanhecer, encontre motivos para sorrir, aprenda com os desafios e abrace a oportunidade de fazer a diferença, pois é na jornada que descobrimos a verdadeira essência da vida.</p>
+            </div><!--card-contet-->
+            <div class="card-action"></div>
+          </div><!--card-->
+       </div><!--col-s12-m6-->
+     </div><!--row-->
+    </div><!--container-->
+  </div><!--section-->
+
+    <div class="section">
+
+      <div class="container">
+            <div class="row">
+                <div class="custom-title">
+                      <div></div>
+                     <h2>Serviços!</h2>
+                  </div>
+
+                  <div class="col m4 s12">
+                      <div class="card">
+                          <div class="card-image">
+                              <img src="images/carrolavado.jpeg" />
+                              <span class="card-title">Lavagem Detalhada</span>
+                          </div><!--card-image-->
+                          <div class="card-content">
+                        <p>Atendimento personalizado e meticuloso: a Limpeza Detalhada proporciona um cuidado especializado e aprofundado para cada parte do seu veículo.</p>
+                          </div>
+                      </div><!--card-small-->
+                  </div><!--col-m4-s12-->
+
+                  <div class="col m4 s12">
+                      <div class="card">
+                          <div class="card-image">
+                              <img src="images/carropaisagem.jpg" />
+                              <span class="card-title">Higienização Interna</span>
+                          </div><!--card-image-->
+                          <div class="card-content">
+                        <p>Ambiente interno saudável e agradável: a higienização e a oxi-sanitização garantem um ambiente interno mais limpo e livre de odores, promovendo maior conforto e bem-estar para os passageiros.</p>
+                          </div>
+                      </div><!--card-small-->
+                  </div><!--col-m4-s12-->
+
+                   <div class="col m4 s12">
+                       <div class="card">
+                          <div class="card-image">
+                              <img src="images/motolavada.jpeg" />
+                              <span class="card-title">Limpeza Estética</span>
+                          </div><!--card-image-->
+                          <div class="card-content">
+                        <p>Conservação e valorização do veículo: a atenção aos detalhes e a aplicação de produtos específicos contribuem para a preservação do veículo, aumentando seu valor de mercado e prolongando sua vida útil.</p>
+                          </div>
+                      </div><!--card-small-->
+                  </div><!--col-m4-s12-->
+
+            </div><!--row-->
+      </div><!--container-->
+    </div><!--section-->
+
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col m6 s12">
+            <div class="custom-title">
+              <h2>Equipe!</h2>
+            </div><!--custom-title-->
+             <ul class="collection">
+               <li class="collection-item avatar">
+                 <i class="material-icons circle">assignment</i>
+                   <span class="title">Marcos Ursine</span>
+                     <p>Founder/CEO </p>
+                     <a href="#!" class="secondary-content"><i class="material-icons">assignment_ind</i></a>
+               </li>
+             </ul>
+          </div><!--col-m6-s12-->
+
+          <div class="col m6 s12">
+            <div class="custom-title">
+              <h2>Equipe!</h2>
+            </div><!--custom-title-->
+             <ul class="collection">
+               <li class="collection-item avatar">
+                 <i class="material-icons circle">assignment</i>
+                   <span class="title">Marcos Ursine</span>
+                     <p>Polishing Technician</br>
+                     Technical and detailed cleaning
+                     </p>
+                     <a href="#!" class="secondary-content"><i class="material-icons">assignment_ind</i></a>
+               </li>
+             </ul>
+          </div><!--col-m6-s12-->
+
+        </div><!--row-->
+      </div><!--container-->
+    </div><!--section-->
+
+    <div class="section">
+      <div class="container">
+        <div class="custom-title">
+          <div></div>
+          <h2>Entre Contato</h2>
+        </div><!--custom-title-->
+        <br/>
+        <div class="container" style="max-width: 800px;">
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12 m6">
+                  <input type="text" name="first_name" class="validate">
+                  <label for="first_name">Primeiro nome</label>
+                </div><!--input-field-col-s12-m6-->
+
+                <div class="input-field col s12 m6">
+                  <input type="text" name="last_name" class="validate">
+                  <label for="last_name">Segundo nome</label>
+                </div><!--input-field-col-s12-m6-->
+              </div><!--row-->
+
+              <div class="row">
+                <div class="input-field col s12">
+                  <input type="email" id="email" class="validate">
+                  <label for="email">E-mail</label>
+                </div><!--input-field-col-s12-m6-->
+              </div><!--row-->
+
+               <div class="row">
+                <div class="input-field col s12">
+                  <textarea id="textarea1" class="materialize-textarea"></textarea>
+                  <label for="textarea1">Mensagem</label>
+                </div><!--input-field-col-s12-->
+              </div><!--row-->
+
+               <div class="row">
+                       <button class="btn waves-effect waves-light light-blue darken-2 pulse" type="submit" name="action">Enviar
+                            <i class="material-icons right">send</i>
+                        </button>
+                  </div><!--row-->
+
+            </form><!--col--s12-->
+          </div><!--row-->
+        </div><!--container-->
+      </div><!--container-->
+    </div><!--section-->
+
+    <footer class="page-footer light-blue darken-2">
+          <div class="container">
+            <div class="row">
+              <div class="col m6 s12">
+                <h5 class="white-text">A Empresa</h5>
+                <p class="grey-text text-lighten-4">Que tal cuidar do seu carro enquanto trabalha, descansa ou curte o dia? Seu veículo pode ser atendido em um Ponto Ursin´s Wash ou na sua garagem. Sem sujeira, com segurança e qualidade.</p>
+              </div>
+              <div class="col m4 offset-m2 s12">
+                <h5 class="white-text">Suporte ao Cliente</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Agendamento</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Contato</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Dúvidas Frequentes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2022 Todos os direitos
+            <a class="grey-text text-lighten-4 right" href="https://www.instagram.com/ursiniswash/">Redes Socias</a>
+            </div>
+          </div>
+        </footer>
+
+    <!--Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+</body>
+</html>
